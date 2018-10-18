a41427bd81b31d04bfdf55bc343893c0e229ab79
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-18 14:44:26 +0200

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -5,17 +5,24 @@ var Contact = React.createClass({
 
   render: function() {
     return (
-      React.createElement('div', {className: 'contactItem'},
-        React.createElement('img', {
-          className: 'contactImage',
-          src: 'http://icons.veryicon.com/png/System/100%20Flat%20Vol.%202/contacts.png'
-        }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-          this.props.item.email
-        )
+      <div className={'contactItem'}>
+      <img className={'contactImage'} src={'http://icons.veryicon.com/png/System/100%20Flat%20Vol.%202/contacts.png'}/>
+        <p className={'contactLabel'}>
+        Imię: {this.props.item.firstName}
+        </p>
+        <p className={'contactLabel'}>
+        Nazwisko: {this.props.item.firstName}
+        </p>
+       <a href={'mailto:' + this.props.item.email}>
+        {this.props.item.email}
+        </a>
+      </div>
       )
     )
   },
 });
+
+
+
+    
+   
