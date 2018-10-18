c78996d0613325ef6bfd124845a0e0897a03a323
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-10-18 14:46:53 +0200

    Update App.js

--- components/App.js
+++ components/App.js
@@ -25,13 +25,14 @@ var contactForm = {
   email: ''
 };
 
+
 var App = React.createClass({
-  render: function() {
-    return (
-      React.createElement('div', {className: 'app'},
-        React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {})
-      )
-    );
-  }
+    render: function() {
+        return (
+            <div className = { 'app' } > 
+                <ContactForm contact = { contactForm } ></ContactForm>
+                <Contacts items = { contacts } ></Contacts>
+            </div>
+        );
+    }
 });
